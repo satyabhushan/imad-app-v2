ba09b6228d49517336f9dff025e47f6dc7b6a816
satyabhushan <satya73989@gmail.com>
2017-04-04 13:23:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/trackUserAction/:url',function(req,res){
                 console.log(err);
             }else{
                 if(result.rows.length === 0){
-                    var tot = 1, ind = (url == 'ind' ? 1 : 0), top = (url == 'top' ? 1 : 0), srch = (url == 'srch' ? 1 : 0), abt = (url == 'abt' ? 1 : 0), lout = (url == 'lout' ? 1 : 0), loin = (url == 'loin' ? 1 : 0);
+                    var tot = 1, ind = (url == 'index' ? 1 : 0), top = (url == 'topic' ? 1 : 0), srch = (url == 'search' ? 1 : 0), abt = (url == 'aboutme' ? 1 : 0), lout = (url == 'logout' ? 1 : 0), loin = (url == 'login' ? 1 : 0);
                     pool.query('INSERT INTO "visiters" ("userip","totalvisit","indexvisit","topicvisit","searchvisit","aboutmevisit","logoutvisit","loginvisit") VALUES ($1, $2,$3,$4,$5,$6,$7,$8)',[req.connection.remoteAddress,tot,ind,top,srch,abt,lout,loin],function(err,result){
                         if(err){
                             console.log(err);
@@ -83,7 +83,7 @@ app.get('/trackUserAction/:url',function(req,res){
                         }
                     });
                 }else{
-                    var tot = result.rows[0].totalvisit , ind = (url == 'ind' ? result.rows[0].indexvisit + 1 : result.rows[0].indexvisit), top = (url == 'top' ? result.rows[0].topicvisit + 1 : result.rows[0].topicvisit), srch = (url == 'srch' ? result.rows[0].topicvisit + 1 : result.rows[0].searchvisit), abt = (url == 'abt' ? result.rows[0].aboutmevisit +1 : result.rows[0].aboutmevisit), lout = (url == 'lout' ? result.rows[0].logoutvisit : result.rows[0].logoutvisit), loin = (url == 'loin' ? result.rows[0].loginvisit : result.rows[0].loginvisit);
+                    var tot = result.rows[0].totalvisit , ind = (url == 'index' ? result.rows[0].indexvisit + 1 : result.rows[0].indexvisit), top = (url == 'topic' ? result.rows[0].topicvisit + 1 : result.rows[0].topicvisit), srch = (url == 'search' ? result.rows[0].topicvisit + 1 : result.rows[0].searchvisit), abt = (url == 'aboutme' ? result.rows[0].aboutmevisit +1 : result.rows[0].aboutmevisit), lout = (url == 'logout' ? result.rows[0].logoutvisit : result.rows[0].logoutvisit), loin = (url == 'login' ? result.rows[0].loginvisit : result.rows[0].loginvisit);
                     pool.query('update "users" set totalvisit = $1, indexvisit = $2,topicvisit = $3,searchvisit = $4, aboutmevisit = $5,logoutvisit = $6,loginvisit = $7 where userip = $8',[tot,ind,top,srch,abt,lout,loin,req.connection.remoteAddress],function(err,result){
                         if(err){
                             console.log(err);
